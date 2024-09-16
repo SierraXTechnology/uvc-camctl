d5125143b18ea2200a86f27d5103a3fe98e3b3c4
mark081 <1995143-mark081@users.noreply.gitlab.com>
2024-09-16 10:20:50 -0700

    Updated deprecated dependencies

--- uvc-camctl.js
+++ uvc-camctl.js
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2024 Sierra X Consulting
+ * Copyright (c) 2024 Mark Cooper - Sierra X Consulting Inc. (mark@sierraxtech.com)
+ * Licensed under the MIT license.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
